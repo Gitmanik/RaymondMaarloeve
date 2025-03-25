fe4f8522ecb6c6aa540fcec07c166d6be845fe5b
paweldol2003 <paweldolak2003@gmail.com>
2025-03-25 18:51:54 +0100

    small buildings reorganization

--- RaymondMaarloeve/Assets/Scripts/TerrainGenerator.cs
+++ RaymondMaarloeve/Assets/Scripts/TerrainGenerator.cs
@@ -1,100 +1,151 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class TerrainGenerator : MonoBehaviour
 {
     public static TerrainGenerator Instance { get; private set; }
-    
+
     public int width = 100, height = 100;
-    // public int sqrtLayerResolution = 10;
-
-    public Terrain terrain;
-    
-    // public Texture2D terrainTexture;
-
     public int buildingArea = 10;
     public GameObject[] Buildings;
+    public Terrain terrain;
+
+    public List<GameObject> spawnedBuildings = new List<GameObject>();
 
     void Awake()
     {
         Instance = this;
     }
-    
+
     public void GenerateMap()
     {
         Debug.Log("Generating map");
-        width = (int) Math.Floor(terrain.terrainData.size.x);
-        height = (int) Math.Floor(terrain.terrainData.size.z);
 
-        var terrainData = terrain.terrainData;
-        
-        // TerrainData terrainData = new TerrainData
-        // {
-        //     heightmapResolution = width + 1,
-        //     size = new Vector3(width, 1, height)
-        // };
-        //
-        // float[,] heights = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
-        // terrainData.SetHeights(0, 0, heights);
-        //
-        // terrain.terrainData   = terrainData;
-        //
-        // if (terrainTexture)
-        // {
-        //     TerrainLayer[] layers = new TerrainLayer[sqrtLayerResolution * sqrtLayerResolution];
-        //
-        //     for (int i = 0; i < sqrtLayerResolution * sqrtLayerResolution; i++)
-        //     {
-        //         layers[i] = new TerrainLayer
-        //         {
-        //             diffuseTexture = terrainTexture,
-        //             tileSize = new Vector2(width / sqrtLayerResolution, height / sqrtLayerResolution) // Dzielenie na 9 kwadrat�w
-        //         };
-        //     }
-        //
-        //     terrainData.terrainLayers = layers;
-        //     
-        //     float[,,] alphaMap = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, sqrtLayerResolution * sqrtLayerResolution];
-        //     int cellSizeX = terrainData.alphamapWidth / sqrtLayerResolution;
-        //     int cellSizeY = terrainData.alphamapHeight / sqrtLayerResolution;
-        //
-        //     for (int x = 0; x < terrainData.alphamapWidth; x++)
-        //     {
-        //         for (int y = 0; y < terrainData.alphamapHeight; y++)
-        //         {
-        //             int textureIndex = (x / cellSizeX) + (y / cellSizeY) * sqrtLayerResolution;
-        //             for (int i = 0; i < sqrtLayerResolution * sqrtLayerResolution; i++)
-        //             {
-        //                 alphaMap[x, y, i] = (i == textureIndex) ? 1f : 0f; // Ka�dy kwadrat trawy ma swoj� warstw�
-        //             }
-        //         }
-        //     }
-        //
-        //     terrainData.SetAlphamaps(0, 0, alphaMap);
-        // }
-        // else
-        // {
-        //     Debug.LogError("Musisz przypisa� tekstur� do 'terrainTexture'!");
-        // }
-        //
+        width = (int)Math.Floor(terrain.terrainData.size.x);
+        height = (int)Math.Floor(terrain.terrainData.size.z);
+
+        TerrainData terrainData = terrain.terrainData;
+
+        spawnedBuildings.Clear();
 
         for (int x = 0; x < terrainData.size.x; x += buildingArea)
         {
             for (int z = 0; z < terrainData.size.z; z += buildingArea)
             {
-                Vector3 position = new Vector3(transform.position.x -width/2 + x + buildingArea / 2, 0, transform.position.z*2 -height/2 + z + buildingArea / 2);
+                Vector3 position = new Vector3(
+                    transform.position.x - width / 2 + x + buildingArea / 2,
+                    0,
+                    transform.position.z * 2 - height / 2 + z + buildingArea / 2
+                );
 
-                int randomIndex = Random.Range(0, 100); // potrzebny lepszy system losowanie z wieloma warunkami
+                int randomIndex = Random.Range(0, 100);
                 GameObject result = randomIndex switch
                 {
                     int n when (n >= 0 && n <= 3) => Instantiate(Buildings[0], position, Quaternion.identity, terrain.transform),
                     int n when (n >= 4 && n <= 13) => Instantiate(Buildings[1], position, Quaternion.identity, terrain.transform),
                     int n when (n >= 14 && n <= 24) => Instantiate(Buildings[2], position, Quaternion.identity, terrain.transform),
-
+                    int n when (n >= 25 && n <= 34) => Instantiate(Buildings[3], position, Quaternion.identity, terrain.transform),
                     _ => null
                 };
-                
+
+                if (result != null)
+                {
+                    spawnedBuildings.Add(result);
+                }
+            }
+        }
+
+        DrawPathsBetweenBuildings();
+    }
+
+    void DrawPathsBetweenBuildings()
+    {
+        TerrainData terrainData = terrain.terrainData;
+
+        int mapWidth = terrainData.alphamapWidth;
+        int mapHeight = terrainData.alphamapHeight;
+        int numTextures = terrainData.alphamapLayers;
+
+        if (numTextures < 2)
+        {
+            Debug.LogWarning("Potrzebujesz co najmniej 2 tekstur w Terrain Layers: np. trawa i ścieżka.");
+            return;
+        }
+
+        float[,,] alphaMap = terrainData.GetAlphamaps(0, 0, mapWidth, mapHeight);
+
+        foreach (var from in spawnedBuildings)
+        {
+            GameObject to = GetClosestBuilding(from);
+
+            if (to != null)
+            {
+                Vector3 worldStart = from.transform.position;
+                Vector3 worldEnd = to.transform.position;
+
+                Vector3 alphaStart = WorldToAlphaMapCoord(worldStart, terrainData);
+                Vector3 alphaEnd = WorldToAlphaMapCoord(worldEnd, terrainData);
+
+                int steps = 100;
+                float pathWidth = 3f;
+
+                for (int i = 0; i <= steps; i++)
+                {
+                    float t = i / (float)steps;
+                    Vector3 pos = Vector3.Lerp(alphaStart, alphaEnd, t);
+                    PaintCircleOnAlphaMap(alphaMap, (int)pos.x, (int)pos.z, pathWidth, 1, numTextures);
+                }
+            }
+        }
+
+        terrainData.SetAlphamaps(0, 0, alphaMap);
+    }
+
+    Vector3 WorldToAlphaMapCoord(Vector3 worldPos, TerrainData terrainData)
+    {
+        Vector3 terrainPos = worldPos - terrain.transform.position;
+        float x = (terrainPos.x / terrainData.size.x) * terrainData.alphamapWidth;
+        float z = (terrainPos.z / terrainData.size.z) * terrainData.alphamapHeight;
+        return new Vector3(x, 0, z);
+    }
+
+    GameObject GetClosestBuilding(GameObject from)
+    {
+        GameObject closest = null;
+        float minDist = Mathf.Infinity;
+
+        foreach (var other in spawnedBuildings)
+        {
+            if (other == from) continue;
+            float dist = Vector3.Distance(from.transform.position, other.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                closest = other;
+            }
+        }
+
+        return closest;
+    }
+
+    void PaintCircleOnAlphaMap(float[,,] alphas, int centerX, int centerZ, float radius, int texIndex, int numTextures)
+    {
+        int r = Mathf.CeilToInt(radius);
+        for (int z = -r; z <= r; z++)
+        {
+            for (int x = -r; x <= r; x++)
+            {
+                int px = centerX + x;
+                int pz = centerZ + z;
+                if (px < 0 || pz < 0 || px >= alphas.GetLength(0) || pz >= alphas.GetLength(1)) continue;
+
+                if (x * x + z * z <= radius * radius)
+                {
+                    for (int i = 0; i < numTextures; i++)
+                        alphas[px, pz, i] = (i == texIndex) ? 1f : 0f;
+                }
             }
         }
     }
