c9b1e3dcefa7a52effd923cdf0b2e646cdc660f2
Marek Nijakowski <129692083+Ariuscc@users.noreply.github.com>
2025-05-12 21:54:48 +0200

    #30 Minigame UI prototype
    - added few panels and buttons
    - upper row for history blocks pool
    - lower row for history blocks sequence
    - Minigame will have draggable history  blocks from "murder log" in which there will be full story of the murder divided into these blocks. Player will have to put blocks into a correct sentence and point out the culprit/murderer.

---
+++ RaymondMaarloeve/Assets/Scripts/SlotDropHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotDropHandler : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        var dragged = eventData.pointerDrag;
+        if (dragged != null)
+        {
+            dragged.transform.SetParent(transform);
+            var rt = dragged.GetComponent<RectTransform>();
+            rt.anchoredPosition = Vector2.zero;
+        }
+    }
+}
