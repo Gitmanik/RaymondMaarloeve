21846f7d77c8afd700a97e956acb0b459c42a8ef
Michał Eisler <162569539+Ic3Man1@users.noreply.github.com>
2025-06-11 18:19:48 +0200

    Change NPC names for the real ones

--- RaymondMaarloeve/Assets/Scripts/NPC/NpcIdentity.cs
+++ RaymondMaarloeve/Assets/Scripts/NPC/NpcIdentity.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
+[RequireComponent(typeof(NPC))]
 public class NpcIdentity : MonoBehaviour
 {
     public string npcName = "Unnamed";
+
+    void Start()
+    {
+        NPC npc = GetComponent<NPC>();
+        if (npc != null)
+        {
+            npcName = npc.NpcName;
+        }
+        else
+        {
+            Debug.LogWarning("NpcIdentity: Brak komponentu NPC przy obiekcie " + gameObject.name);
+        }
+    }
 }
